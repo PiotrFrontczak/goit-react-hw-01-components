cc46d7c43b05bd7631a0dff36d751bd9c3a626b7
Piotr Frontczak <piotrf2023@gmail.com>
2024-06-02 22:50:32 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
-import Stats from "./components/Stats.jsx"
+import Stats from "./components/Stats.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <Stats/>
+    <Stats />
   </React.StrictMode>,
 );
