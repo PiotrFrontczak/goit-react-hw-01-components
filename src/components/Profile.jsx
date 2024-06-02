15cdb7a450cc2e1ded5814e1b27a330af67b2760
Piotr Frontczak <piotrf2023@gmail.com>
2024-06-02 15:30:58 +0200

    update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,53 +1,46 @@
 import { Component } from "react";
 import PropTypes from "prop-types";
 
-
 export default class Profile extends Component {
-    static defaultProps = {
+  static defaultProps = {};
 
-    }
+  static PropTypes = {
+    username: PropTypes.string,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    avatar: PropTypes.string,
+    stats: PropTypes.shape({
+      followers: PropTypes.number,
+      views: PropTypes.number,
+      likes: PropTypes.number,
+    }),
+  };
 
-    static PropTypes = {
-        username: PropTypes.string,
-        tag: PropTypes.string,
-        location: PropTypes.string,
-        avatar: PropTypes.string,
-        stats: PropTypes.shape({
-            followers: PropTypes.number,
-            views: PropTypes.number,
-            likes: PropTypes.number,
-        }),
-    }
+  render() {
+    return (
+      <div class="profile">
+        <div class="description">
+          <img src={this.props.avatar} alt="User avatar" class="avatar" />
+          <p class="name">{this.props.username}</p>
+          <p class="tag">{this.props.tag}</p>
+          <p class="location">{this.props.location}</p>
+        </div>
 
-    render() {
-        return (
-            <div class="profile">
-                <div class="description">
-                    <img
-                        src={this.props.avatar}
-                        alt="User avatar"
-                        class="avatar"
-                    />
-                    <p class="name">{this.props.username}</p>
-                    <p class="tag">{this.props.tag}</p>
-                    <p class="location">{this.props.location}</p>
-                </div>
-
-                <ul class="stats">
-                    <li>
-                        <span class="label">Followers</span>
-                        <span class="quantity">{this.props.stats.followers}</span>
-                    </li>
-                    <li>
-                        <span class="label">Views</span>
-                        <span class="quantity">{this.props.stats.views}</span>
-                    </li>
-                    <li>
-                        <span class="label">Likes</span>
-                        <span class="quantity">{this.props.stats.likes}</span>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+        <ul class="stats">
+          <li>
+            <span class="label">Followers</span>
+            <span class="quantity">{this.props.stats.followers}</span>
+          </li>
+          <li>
+            <span class="label">Views</span>
+            <span class="quantity">{this.props.stats.views}</span>
+          </li>
+          <li>
+            <span class="label">Likes</span>
+            <span class="quantity">{this.props.stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
