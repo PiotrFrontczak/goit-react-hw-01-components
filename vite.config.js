0ceae8e502fddbd7c832d17483ed4c3feb897aab
Piotr Frontczak <piotrf2023@gmail.com>
2024-06-02 21:22:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
@@ -7,4 +7,18 @@ export default defineConfig({
   build: {
     outDir: "./dist",
   },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      },
+    }
+},
 });
