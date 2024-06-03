02c6a91af15f4808b4ec65fe366b5c7af9993e73
Piotr Frontczak <piotrf2023@gmail.com>
2024-06-03 13:44:04 +0200

    transactions added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./components/App.jsx";
 import Stats from "./components/Stats.jsx";
 import FriendList from "./components/FriendList.jsx";
 import friends from "./data/friends.json";
+import TransactionHistory from "./components/TransactionHistory.jsx";
+import transactions from "./data/transactions.json";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Stats />
     <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
   </React.StrictMode>,
 );
