5ae166201464fea4638cc79cfd8898adce0d6e21
Piotr Frontczak <piotrf2023@gmail.com>
2024-06-03 12:42:51 +0200

    component friend added

---
+++ src/components/FriendList.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './FriendList.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className="item">
+      <span className={`status ${isOnline ? 'online' : 'offline'}`}></span>
+      <img className="avatar" src={avatar} alt={`${name} avatar`} width="48" />
+      <p className="name">{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
 import Stats from "./components/Stats.jsx";
+import FriendList from "./components/FriendList.jsx";
+import friends from "./data/friends.json";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Stats />
+    <FriendList friends={friends} />
   </React.StrictMode>,
 );
